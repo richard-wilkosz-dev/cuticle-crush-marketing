33d31c8bfd61d0ee661853e4a00586e1297eebf5
Richard <33876882+richardwilkosz@users.noreply.github.com>
2026-08-10 22:34:31 -0500

    Prevent bad SEO during development

--- src/components/coming-soon-page.tsx
+++ src/components/coming-soon-page.tsx
@@ -1,7 +1,8 @@
 export function ComingSoonPage() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <span className="text-3xl font-semibold">Coming soon...</span>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-semibold">Cuticle Crush</h1>
+      <h2 className="text-2xl text-muted-foreground">Coming soon...</h2>
     </div>
   )
 }
