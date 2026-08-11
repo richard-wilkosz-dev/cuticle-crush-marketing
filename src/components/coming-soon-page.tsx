a83fd2b402d2b718d9ec5942bdea094f6542629e
Richard <33876882+richardwilkosz@users.noreply.github.com>
2026-08-10 20:15:03 -0500

    Add Coming Soon page

---
+++ src/components/coming-soon-page.tsx
@@ -0,0 +1,7 @@
+export function ComingSoonPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <span className="text-3xl font-semibold">Coming soon!</span>
+    </div>
+  )
+}
