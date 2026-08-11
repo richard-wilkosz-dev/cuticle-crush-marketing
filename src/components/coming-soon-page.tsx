95a2d0c3ece50eb2d8b8aa9336bfdc9da8168410
Richard <33876882+richardwilkosz@users.noreply.github.com>
2026-08-10 20:50:11 -0500

    Stub

--- src/components/coming-soon-page.tsx
+++ src/components/coming-soon-page.tsx
@@ -1,7 +1,7 @@
 export function ComingSoonPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <span className="text-3xl font-semibold">Coming soon!</span>
+      <span className="text-3xl font-semibold">Coming soon...</span>
     </div>
   )
 }
